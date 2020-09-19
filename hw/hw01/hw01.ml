de58b2c51fc547278a2bd850e619168fceb4dd8b
Junwei, Chen <junwei.chen@downtown8.com>
2020-09-19 17:25:48 -0400

    feat: add type check

--- hw/hw01/hw01.ml
+++ hw/hw01/hw01.ml
@@ -5,7 +5,8 @@ let rev_square = [0; 3; 3; 2; 2; 5; 5; 4; 4; 1]
 let counter_square = [0;2;2;3;3;5;5;4;4;1]
 let letter_e : program = [0; 2; 2; 3; 3; 5; 5; 4; 3; 5; 4; 3; 3; 5; 5; 1]
 
-let rec colored_help (x,y) l = 
+let rec colored_help: (int*int) -> program -> (int * int) list = 
+  fun (x,y) l ->
   match l with
   | [] -> []
   | h::t -> 
@@ -16,26 +17,30 @@ let rec colored_help (x,y) l =
     | 5 -> [(x - 1, y)] @ colored_help (x - 1, y) t
     | _ -> [(x, y)] @ colored_help(x, y) t
 
-let rec rem_dup l =
+let rec rem_dup: (int*int) list -> (int*int) list = 
+  fun l ->
   match l with
   | [] -> []
   | x::t -> if List.mem x t then rem_dup t else x:: rem_dup t
 
-let colored coor image = rem_dup @@ colored_help coor image
+let colored: (int*int) -> program -> (int*int) list =
+  fun coor image -> rem_dup @@ colored_help coor image
 
 
 (* 2. equ when two programs have same set of coordinates. *)
-let helper f x= List.sort compare @@ List.map f (colored (0, 0) x)
 
 let equivalent a b: bool =
-  let a_x = helper fst a in
-  let a_y = helper snd a in
-  let b_x = helper fst b in
-  let b_y = helper snd b in
+  let equ_helper: ('a * 'a -> 'a) -> program -> int list = 
+    fun f x -> List.sort compare @@ List.map f (colored (0, 0) x)
+  in
+  let a_x = equ_helper fst a in
+  let a_y = equ_helper snd a in
+  let b_x = equ_helper fst b in
+  let b_y = equ_helper snd b in
   a_x = b_x && a_y = b_y
 
 (* 3. returns a program that draws the mirror image *)
-let mirror_image' x =
+let mirror_helper: int -> int = fun x ->
   match x with
   | 2 -> 4
   | 4 -> 2
@@ -43,11 +48,10 @@ let mirror_image' x =
   | 5 -> 3
   | _ -> x
 
-let mirror_image image = List.map mirror_image' image
-
+let mirror_image: program -> program = fun image -> List.map mirror_helper image
 (* 4. rotate image 90 degrees*)
 
-let rotate_image_90_helper x =
+let rotate_image_90_helper:int -> int = fun x ->
   match x with
   | 2 -> 3
   | 3 -> 4
@@ -55,37 +59,38 @@ let rotate_image_90_helper x =
   | 5 -> 2
   | _ -> x
 
-let rotate_image_90 images = List.map rotate_image_90_helper images
+let rotate_image_90: program -> program = fun images ->
+  List.map rotate_image_90_helper images
 
 (* 5. repeat function *)
-let rec repeat (r_times:int) x =
+let rec repeat (r_times:int) (x:'a): 'a list =
   match r_times with
   | 0 -> []
   | _ -> x :: repeat (r_times - 1) x
 
 (* 6. enlarge program x times *)
-let rec pantograph (x:int) (images:program) =
+let rec pantograph (x:int) (images:program): program =
   match images with
   | [] -> []
   | h::t -> if h = 0 || h = 1 then [h] @ pantograph x t else repeat x h @ pantograph x t
 
-let pantograph_m_helper x_times item =
+let pantograph_m_helper (x_times:int) (item:int): int list =
   match item with
   | 0 -> [item]
   | 1 -> [item]
   | _ -> repeat x_times item
 
-let pantograph_m (enlarge_times:int) (images:program) = List.flatten @@ List.map (pantograph_m_helper enlarge_times) images
+let pantograph_m (enlarge_times:int) (images:program): program = List.flatten @@ List.map (pantograph_m_helper enlarge_times) images
 
 (* 6.2 pantograph_fold *)
 
-let pantograph_f_helper x_times item acc =
+let pantograph_f_helper: int -> int -> int list -> int list = fun x_times item acc ->
   match item with
   | 0 -> item :: acc
   | 1 -> item :: acc
   | _ -> repeat x_times item @ acc
 
-let pantograph_f (enlarge_times:int) (images:program) = 
+let pantograph_f (enlarge_times:int) (images:program): program = 
   List.fold_right (pantograph_f_helper enlarge_times) images []
 
 let rec compress_helper image = 
@@ -97,13 +102,14 @@ let rec compress_helper image =
     then compress_helper ((x, y+y_1)::l)
     else (x,y) :: compress_helper((x_1, y_1)::l)
 
-let compress image = 
-  compress_helper (List.map (fun x -> (x, 1)) image)
+let compress: program -> (int * int) list =
+  fun image -> compress_helper (List.map (fun x -> (x, 1)) image)
 (* 8. uncpmress *)
-let rec uncompress image =
-  match image with
-  | [] -> []
-  | (x, y)::t -> repeat y x @ uncompress t 
+let rec uncompress: (int * int) list -> program =
+  fun image ->
+    match image with
+    | [] -> []
+    | (x, y)::t -> repeat y x @ uncompress t 
 
 let uncompress_m_helper (x, y) = repeat y x
 
